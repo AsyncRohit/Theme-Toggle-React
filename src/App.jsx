a68ddcb261eb4cb5727c06e0ceea3edf378e2ad1
Rohit Tomar <heartstealarrohitthakur@gmail.com>
2024-12-26 02:39:00 +0530

    default theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,50 +2,50 @@ import React, { useEffect, useRef, useState } from 'react'
 
 const App = () => {
 
-
-  const [theme, setTheme] = useState(localStorage.getItem('theme'));
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light')
 
   const mainRef = useRef(null)
-  useEffect(function(){
-    mainRef.current.setAttribute('id',theme);
+  
+  
 
-    },[theme])
 
-  const ThemeChanger=()=>{
-      if(theme == 'light'){
-        setTheme('dark');
-        localStorage.setItem('theme','dark')
-      }
-      else{
-        setTheme('light');
-        localStorage.setItem('theme','light')
-      }
-      console.log(theme);
+  const themeChanger=()=>{
+    if(theme =='light'){
+      setTheme('dark')
+      localStorage.setItem('theme','dark')
+    }
+    else{
+      setTheme('light')
+      localStorage.setItem('theme','light')
+    }
+
   }
 
-
-
-
+  useEffect(function(){
+    mainRef.current.setAttribute('id',theme)
+  })
+  
   return (
-    <div ref={mainRef} className='h-screen w-full text-white flex justify-center items-center'>
-
-<div className='px-6 py-4  flex justify-between itmes-center w-full fixed top-0 left-0 text-2xl font-semibold '>
-        <h1>Porfolio</h1>
-        <div className=' flex gap-10'>
-            <h1>About</h1>
-            <h1>Services</h1>
-            <h1>Contact</h1>
-            <h1 onClick={()=>{
-                ThemeChanger()
-            }}
-             className='cursor-pointer'
-             >Theme</h1>
-            </div> 
-
-
-    </div>   
-            <h1 className='text-7xl font-bold'>Porfolio</h1>
-     </div>
+    <div 
+    ref={mainRef}
+    className='w-full h-screen flex justify-center items-center'> 
+      <div className=' navbar px-6 py-4 flex justify-between  items-center fixed top-0 left-0 w-full text-2xl font-semibold'>
+        <h1>My dream company</h1>
+         <div className='flex gap-20'>
+          <h1>About</h1>
+          <h1>Contact</h1>
+          <h1>Servies</h1>
+          <h1 
+           onClick={()=>{
+            themeChanger()
+                }}
+          className='cursor-pointer'>Theme</h1>
+         </div>
+      </div>
+      <h1 
+     
+      className='text-7xl font-bold'> My Dream company.Com</h1>
+    </div>
   )
 }
 
